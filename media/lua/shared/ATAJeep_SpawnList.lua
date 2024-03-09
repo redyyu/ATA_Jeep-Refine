30d93d9394465f46fcb32852a8e49ea01ac946ee
redyyu <redy.ru@gmail.com>
2024-03-09 18:40:49 +0800

    xxx

--- media/lua/shared/ATAJeep_SpawnList.lua
+++ media/lua/shared/ATAJeep_SpawnList.lua
@@ -1,14 +1,18 @@
 VehicleZoneDistribution = VehicleZoneDistribution or {};
 
-VehicleZoneDistribution.parkingstall.vehicles["Base.ATAJeepClassic"] = {index = -1, spawnChance = 5};
-VehicleZoneDistribution.medium.vehicles["Base.ATAJeepClassic"] = {index = 4, spawnChance = 5};
-VehicleZoneDistribution.good.vehicles["Base.ATAJeepClassic"] = {index = 0, spawnChance = 5};
-VehicleZoneDistribution.ranger.vehicles["Base.ATAJeepClassic"] = {index = 6, spawnChance = 5};
+VehicleZoneDistribution.parkingstall.vehicles["Base.ATAJeepClassic"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.medium.vehicles["Base.ATAJeepClassic"] = {index = 4, spawnChance = 2};
+VehicleZoneDistribution.good.vehicles["Base.ATAJeepClassic"] = {index = 0, spawnChance = 2};
+VehicleZoneDistribution.ranger.vehicles["Base.ATAJeepClassic"] = {index = 6, spawnChance = 2};
 
-VehicleZoneDistribution.trailerpark.vehicles["Base.ATAJeepArcher"] = {index = 0, spawnChance = 5};
-VehicleZoneDistribution.sport.vehicles["Base.ATAJeepArcher"] = {index = 0, spawnChance = 5};
+VehicleZoneDistribution.trailerpark.vehicles["Base.ATAJeepArcher"] = {index = 0, spawnChance = 2};
+VehicleZoneDistribution.sport.vehicles["Base.ATAJeepArcher"] = {index = 0, spawnChance = 2};
+VehicleZoneDistribution.good.vehicles["Base.ATAJeepArcher"] = {index = 0, spawnChance = 2};
+VehicleZoneDistribution.ranger.vehicles["Base.ATAJeepArcher"] = {index = 6, spawnChance = 2};
+VehicleZoneDistribution.sport.vehicles["Base.ATAJeepArcher"] = {index = 0, spawnChance = 1};
 
-VehicleZoneDistribution.parkingstall.vehicles["Base.ATAJeepRubicon"] = {index = 2, spawnChance = 5};
-VehicleZoneDistribution.medium.vehicles["Base.ATAJeepRubicon"] = {index = 2, spawnChance = 5};
-VehicleZoneDistribution.good.vehicles["Base.ATAJeepRubicon"] = {index = -1, spawnChance = 5};
-VehicleZoneDistribution.sport.vehicles["Base.ATAJeepRubicon"] = {index = 0, spawnChance = 5};
+VehicleZoneDistribution.parkingstall.vehicles["Base.ATAJeepRubicon"] = {index = 2, spawnChance = 2};
+VehicleZoneDistribution.medium.vehicles["Base.ATAJeepRubicon"] = {index = 2, spawnChance = 2};
+VehicleZoneDistribution.good.vehicles["Base.ATAJeepRubicon"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.sport.vehicles["Base.ATAJeepRubicon"] = {index = 0, spawnChance = 1};
+VehicleZoneDistribution.ranger.vehicles["Base.ATAJeepRubicon"] = {index = 6, spawnChance = 2};
