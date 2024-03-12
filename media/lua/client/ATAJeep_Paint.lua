7893b09e2e615b3c6c5899693070984341c2bb92
redyyu <redy.ru@gmail.com>
2024-03-12 12:45:44 +0800

    fix

--- media/lua/client/ATAJeep_Paint.lua
+++ media/lua/client/ATAJeep_Paint.lua
@@ -25,7 +25,7 @@ PaintVehicle.paintWords = function(playerObj, vehicle, newSkinIndex, paintBrush,
         ISWorldObjectContextMenu.transferIfNeeded(playerObj, paintBrush)
         ISWorldObjectContextMenu.transferIfNeeded(playerObj, paintCan)
         ISTimedActionQueue.add(ISPathFindAction:pathToVehicleArea(playerObj, vehicle, "Engine"))
-        ISTimedActionQueue.add(ISPaintVehicleAction:new(playerObj, vehicle, "Engine", newSkinIndex, paintCan, false))
+        ISTimedActionQueue.add(ISPaintVehicleAction:new(playerObj, vehicle, "Engine", newSkinIndex, paintCan))
     end
 end
 
@@ -34,7 +34,7 @@ PaintVehicle.cleanWords = function(playerObj, vehicle, newSkinIndex, sponge, ble
         ISWorldObjectContextMenu.transferIfNeeded(playerObj, sponge)
         ISWorldObjectContextMenu.transferIfNeeded(playerObj, bleach)
         ISTimedActionQueue.add(ISPathFindAction:pathToVehicleArea(playerObj, vehicle, "Engine"))
-        ISTimedActionQueue.add(ISPaintVehicleAction:new(playerObj, vehicle, "Engine", newSkinIndex, bleach, true))
+        ISTimedActionQueue.add(ISPaintVehicleAction:new(playerObj, vehicle, "Engine", newSkinIndex, bleach))
     end
 end
 
@@ -51,7 +51,7 @@ PaintVehicle.doFillMenuOutsideVehicle = function(playerObj, context, vehicle, te
         context:addSubMenu(paintMenuOpt, subMenu)
 
         -- Paint words
-
+        print('----------------12321313have_uses-------------')
         if (vehicle:getSkinIndex()%2) == 0 then
             local paintBrush = playerInv:getFirstTypeRecurse("Paintbrush")
             local paintCan = playerInv:getFirstTypeRecurse(graffitiType)
@@ -74,10 +74,7 @@ PaintVehicle.doFillMenuOutsideVehicle = function(playerObj, context, vehicle, te
                 local have_uses = 0
                 
                 if paintCan then
-                    
-                    have_uses = math.floor(paintCan:getCurrentUses() * 10) -- less 1 unit, otherwise will be the other item as empty.
-                    print(have_uses)
-                    print('----------------have_uses-------------')
+                    have_uses = round(paintCan:getUsedDelta() * 10) -- less 1 unit, otherwise will be the other item as empty.
                     desc_write = desc_write .. PaintVehicle.ghs..getText("Tooltip_Item_"..graffitiType) .. " ".. have_uses .."/1 unit <LINE> "
                 else
                     desc_write = desc_write .. PaintVehicle.bhs..getText("Tooltip_Item_"..graffitiType) .. " ".. have_uses .."/1 unit <LINE> "
